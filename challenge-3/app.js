744c9394a0339beceef0eed8540e53760f28baa0
Shashank-8p <shashank.8pathak@gmail.com>
2026-09-14 10:26:05 +0530

    Solved Challenge 3

--- challenge-3/app.js
+++ challenge-3/app.js
@@ -1,3 +1,21 @@
-/**
- * Write your challenge solution here
- */
+const nameInput = document.getElementById('nameInput')
+const jobInput = document.getElementById('jobInput')
+const ageInput = document.getElementById('ageInput')
+const bioInput = document.getElementById('bioInput')
+const nameDisplay = document.getElementById('nameDisplay')
+const jobDisplay = document.getElementById('jobDisplay')
+const ageDisplay = document.getElementById('ageDisplay')
+const bioDisplay = document.getElementById('bioDisplay')
+
+nameInput.addEventListener('input', function(e){
+nameDisplay.textContent = e.target.value
+})
+ageInput.addEventListener('input', function(e){
+ageDisplay.textContent = e.target.value
+})
+bioInput.addEventListener('input', function(e){
+bioDisplay.textContent = e.target.value
+})
+jobInput.addEventListener('input', function(e){
+jobDisplay.textContent = e.target.value
+})
