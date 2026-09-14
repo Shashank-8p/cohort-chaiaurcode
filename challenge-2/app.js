36d58194ec42e7da0b269d5577741b4c70098171
Shashank-8p <shashank.8pathak@gmail.com>
2026-09-14 06:50:15 +0530

    Solved Challenge 2

--- challenge-2/app.js
+++ challenge-2/app.js
@@ -1,3 +1,17 @@
-/**
- * Write your challenge solution here
- */
+const heading = document.querySelector('#mainHeading');
+const buttonContainer = document.querySelector('.color-buttons');
+
+buttonContainer.addEventListener('click', function(e) {
+  if (e.target.matches('button')) {
+
+    let clickedText = e.target.textContent
+
+    if (clickedText === 'Reset') {
+      heading.style.color = 'black'; 
+    } 
+    else {
+      heading.style.color = clickedText; 
+    }
+    
+  }
+});
